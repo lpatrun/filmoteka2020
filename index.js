a2266139f8bcd62d5da7b72db83d50f27a7ae74e
Luka Patrun <luka.patrun@gmail.com>
2020-02-26 15:51:36 +0100

    Filmovi se učitavaju 4 po 4. Kada su svi učitani, gumb za loadanje nestaje. Osim njega ima i gumb koji otvara prozor sa svim zanrovima u bazi. Pri odabiru zanra, pojavljuje se prvi film iz njihove baze kojeg opisuje taj zanr (sto znaci da se neki film moze vise puta pojaviti prilikom surfanja zanrovima). Dizajn i UI je u rasulu, što je slijedeće na rasporedu. Nedostaje funkcija da se određeni film lajka.

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ getData(); */
     -current movie
 */
 const movieData = {};
+const movieGenresState = {};
+const movieGenresDB = {};
 
 const controlResults = async () => {
     //1 početni query
@@ -21,8 +23,6 @@ const controlResults = async () => {
         //2 novi objekt
         movieData.search = new Search(query);
 
-        //3 priprema ui za rezultate
-
         //4 traži filmove
         await movieData.search.getResults();
         
@@ -31,6 +31,31 @@ const controlResults = async () => {
     }
 }
 
+const genreResults = async () => {
+    const query = 'https://api.themoviedb.org/3/genre/movie/list?api_key=5945a0abd9acd913047172b2e6571d3e';
+
+    if (query){
+        //1 novi objekt
+        movieGenresState.search = new Search(query);
+
+        //2 traži žanrove
+        await movieGenresState.search.getGenresResults();
+
+        //console.log(movieGenresState.search.genre.data.genres);
+        
+    }
+}
+
+const rouletteResults = async (genreId) => {
+    const query = `https://api.themoviedb.org/3/discover/movie?api_key=5945a0abd9acd913047172b2e6571d3e&with_genres=${genreId}`;
+    //console.log(genreId);
+    if(query){
+        movieGenresDB.search = new Search (query);
+        await movieGenresDB.search.getRouleteResults();
+        //console.log(movieGenresDB.search.genreId.data.results);
+        generateOneMovieByGenre(movieGenresDB.search.genreId.data.results);
+    }
+}
 
 class Search {
     constructor (query){
@@ -47,15 +72,58 @@ class Search {
         }   
     }
 
+    async getGenresResults (){
+        try {
+            const gen = await axios(`${this.query}`);
+            this.genre = gen;        
+        }catch (error) {
+            alert(error);
+        } 
+    }
+
+    async getRouleteResults (){
+        try {
+            const genId = await axios(`${this.query}`);
+            this.genreId = genId;        
+        }catch (error) {
+            alert(error);
+        }
+    }
 }
 
-const renderMovie = movie => {
-    console.log(movie);
+controlResults();
+genreResults();
+
+const limitMovieTitle = title => {
+    const newTitle = [];
+    if( title.length > 25){
+        title.split(' ').reduce((acc, cur) =>{
+            if (acc + cur.length <= 25){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        return `${newTitle.join(' ')}...`;
+    }
+    return title;
+}
+
+const limitMovieYear = year => {
+    year.split('');
+    const newYear = [];
+    for (let i = 0; i < 4; i++){
+        newYear[i] = year[i];
+     }
+     return `${newYear.join('')}`;
+}
+
+const renderMovie = (movie, i) => {
     const markup = `
-    <li>
+    <li class="movie"  data-itemid=${i}>
         <div class="movie-img"><img src ="https://image.tmdb.org/t/p/w185/${movie.poster_path}" alt="${movie.title}"></div>
-        <div class="movie-name-year">${movie.title} (${movie.release_date})</div>
+        <div class="movie-name-year">${limitMovieTitle(movie.title)} (${limitMovieYear(movie.release_date)})</div>
         <div class="movie-lang">Language: ${movie.original_language}</div>
+        <div class="movie-rating">Rating: ${movie.vote_average}</div>
     </li>
     `;
     document.querySelector('.render-movies').insertAdjacentHTML('beforeend', markup);
@@ -64,22 +132,137 @@ const renderMovie = movie => {
 let i = 0, j = 3;
 
 const renderMovies = movies => {
+    //console.log(movies);
     for (; i <= j ; i++){
         
-        renderMovie(movies[i]);
+        renderMovie(movies[i], i);
     }
     //movies.forEach(renderMovie);
 }
 
-
-controlResults();
-
 document.querySelector('.load-more').addEventListener('click', e => {
-    console.log('klik');
-    console.log(i +' ' + j);
     e.preventDefault();
     j += 4;
-    console.log(i +' ' + j);
+    console.log('click');
     renderMovies(movieData.search.result);
+    if ( (j + 4) > 20) {
+        let element = document.querySelector('.load-more');
+        element.parentNode.removeChild(element);
+    }
 });
 
+const renderAMovie = (movie, i) => {
+    const markup = `
+    <li class="movie-ol"  data-itemid=${i}>
+        <div class="movie-name-year-ol">${movie.title} (${limitMovieYear(movie.release_date)})</div>
+        <div class="movie-img-ol"><img src ="https://image.tmdb.org/t/p/w780/${movie.backdrop_path}" alt="${movie.title}"></div>
+        <div class="movie-overview-ol">Overview: ${movie.overview}</div>
+        <div class="test-class clear">
+            <div class="movie-rating-ol"><b>Rating:</b> <em>${movie.vote_average}</em></div>
+            <div class="movie-star-rating-ol"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div>
+        </div>
+        <div class="movie-popularity-ol"><b>Popularity:</b> <em>${movie.popularity}</em></div>
+        <div class="movie-lang-ol"><b>Language:</b> <em>${movie.original_language}</em></div>
+        
+    </li>
+    `;
+    document.querySelector('.render-a-movie').insertAdjacentHTML('beforeend', markup);
+}
+
+document.querySelector('.render-movies').addEventListener ('click', e=>{ 
+    const id = e.target.closest('.movie').dataset.itemid;
+    /* let list = document.querySelector('.overlay-content');
+    while (list.hasChildNodes()){
+        list.removeChild(list.firstChild);
+    } */
+    document.getElementById("myNav").style.display = "block";
+    const miniMarkup =`<ul class="render-a-movie"></ul>`;
+    document.querySelector('.overlay-content').insertAdjacentHTML('beforeend', miniMarkup);
+    renderAMovie(movieData.search.result[id], id);
+});
+
+document.querySelector('.closebtn').addEventListener ('click', e=> {
+    document.getElementById("myNav").style.display = "none";
+    let list = document.querySelector('.overlay-content');
+    while (list.hasChildNodes()){
+        list.removeChild(list.firstChild);
+    }
+  });
+
+
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 27 || event.which == 27) {
+        document.getElementById("myNav").style.display = "none";
+        let list = document.querySelector('.overlay-content');
+        while (list.hasChildNodes()){
+            list.removeChild(list.firstChild);
+        }
+    }
+});
+
+document.querySelector('.movie-roulette').addEventListener('click', e => {
+    e.preventDefault();
+    //document.querySelector('.movies-container-wrapper').classList.toggle('hide');
+    //document.querySelector('.load-more').classList.toggle('hide');
+    document.getElementById("myNav").style.display = "block";
+    const miniMarkup =`<form class="genres-form"></form>`;
+    document.querySelector('.overlay-content').insertAdjacentHTML('beforeend', miniMarkup);
+    renderGenres(movieGenresState.search.genre.data.genres);
+    const buttonMarkup =`<button class="genres-form-send-genre">Roll</button>`;
+    document.querySelector('.overlay-content').insertAdjacentHTML('beforeend', buttonMarkup);
+});
+
+const renderGenres = genres => {
+    genres.forEach(renderGenre);
+}
+
+const renderGenre = genre => {
+    const markup = `
+    <input type="radio" name="genre-type" value=${genre.id}>${genre.name}<br>
+    `;
+    document.querySelector('.genres-form').insertAdjacentHTML('beforeend', markup);
+}
+
+
+document.querySelector('.overlay-content').addEventListener ('click', e=>{ 
+    let genreId = 0;
+    if (e.target.matches('.genres-form-send-genre')){
+        var ele = document.getElementsByName('genre-type'); 
+        for(let k = 0; k < ele.length; k++) { 
+            if(ele[k].checked) 
+            //console.log(ele[i].value);
+            genreId = ele[k].value;
+        }
+        deleteGenreList();
+        const miniMarkup =`<ul class="render-a-movie"></ul>`;
+        document.querySelector('.overlay-content').insertAdjacentHTML('beforeend', miniMarkup);
+        rouletteResults(genreId);        
+    }
+    
+});
+
+
+const deleteGenreList = () => {
+    let list = document.querySelector('.overlay-content');
+        while (list.hasChildNodes()){
+            list.removeChild(list.firstChild);
+        }
+}
+
+const generateOneMovieByGenre = rouletteMovie => {
+    const markup = `
+    <li class="movie-ol">
+        <div class="movie-name-year-ol">${rouletteMovie[0].title} (${limitMovieYear(rouletteMovie[0].release_date)})</div>
+        <div class="movie-img-ol"><img src ="https://image.tmdb.org/t/p/w780/${rouletteMovie[0].backdrop_path}" alt="${rouletteMovie[0].title}"></div>
+        <div class="movie-overview-ol">Overview: ${rouletteMovie[0].overview}</div>
+        <div class="test-class clear">
+            <div class="movie-rating-ol"><b>Rating:</b> <em>${rouletteMovie[0].vote_average}</em></div>
+            <div class="movie-star-rating-ol"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div>
+        </div>
+        <div class="movie-popularity-ol"><b>Popularity:</b> <em>${rouletteMovie[0].popularity}</em></div>
+        <div class="movie-lang-ol"><b>Language:</b> <em>${rouletteMovie[0].original_language}</em></div>
+        
+    </li>
+    `;
+    document.querySelector('.render-a-movie').insertAdjacentHTML('beforeend', markup);
+} 
